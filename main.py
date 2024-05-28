b5be373e8b3b304ccf9d0064aac4db44f4a87698
jtriplet01 <jtriplet01@gmail.com>
2024-05-27 21:55:35 -0500

    Added a button to the main loop.

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import customtkinter
 
 app = customtkinter.CTk()
 app.mainloop()
+app.title("Script Portal")
+app.geometry("500x300")
+
+button = customtkinter.CTkButton(app, text="My Button", command=button_callback)
+button.grid(row=0, column=0, padx=20, pady=20)
+
+app.mainloop()
